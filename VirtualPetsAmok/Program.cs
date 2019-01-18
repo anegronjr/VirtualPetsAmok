243b55209ab36f89efe158eaca7e32ad718fd946
Artie Negron <anegroncsu@gmail.com>
2019-01-18 11:34:56 -0500

    Added main menu.

--- VirtualPetsAmok/Program.cs
+++ VirtualPetsAmok/Program.cs
@@ -6,7 +6,8 @@ namespace VirtualPetsAmok
     {
         static void Main(string[] args)
         {
-            Game();  
+            Game();
+            MainMenu();
         }
 
         static void Game()
@@ -15,8 +16,20 @@ namespace VirtualPetsAmok
 
             Console.WriteLine("Name your pet: ");
             pet.Name = Console.ReadLine();
-            Console.WriteLine("What species is your pet: ");
+            Console.WriteLine("You have named your pet " + pet.Name + ". What species is your pet: ");
             pet.Species = Console.ReadLine();
+            Console.WriteLine("Your pet " + pet.Species + ", " + pet.Name + ", has been created. Press any key to continue.");
+            Console.ReadKey();
+            
+        }
+
+        static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("====== Main Menu =====");
+            Console.WriteLine("1. Display Pet Info.");
+
+
         }
     }
 }
