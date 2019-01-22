a7cd50506a9218279a5c4f0e446707b8b321e685
Artie Negron <anegroncsu@gmail.com>
2019-01-22 13:47:50 -0500

    MAJOR GRAPHICAL UPDATE (added dog and cat ascii art).

--- VirtualPetsAmok/Program.cs
+++ VirtualPetsAmok/Program.cs
@@ -15,8 +15,22 @@ namespace VirtualPetsAmok
             Console.Clear();
             Console.WriteLine("Please name your new pet?");
             pet.Name = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine);
             Console.WriteLine("What species is your new pet?");
-            pet.Species = Console.ReadLine();
+            Console.WriteLine("1. Dog");
+            Console.WriteLine("2. Cat");
+            string speciesChoice = Console.ReadLine();
+
+            switch (speciesChoice)
+            {
+                case "1":
+                    pet.Species = "Dog";
+                    break;
+                case "2":
+                    pet.Species = "Cat";
+                    break;
+            }
+
             Console.Clear();
             Console.WriteLine("Congratulations! Your new pet " + pet.Species + ", " + pet.Name + ", is ready to go!");
             Console.WriteLine("Press ENTER to go to the main menu.");
@@ -27,13 +41,37 @@ namespace VirtualPetsAmok
         static void PetInfo() 
         {
             Console.Clear();
+            DrawPicture();
             Console.WriteLine("Pet Name: " + pet.Name);
-            Console.WriteLine("Pet Species: " + pet.Species, Environment.NewLine);
+            Console.WriteLine("Pet Species: " + pet.Species);
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Press ENTER to return to the Main Menu.");
+            Console.ReadKey();
+            MainMenu();
+        }
+
+        static void PetInteractions()
+        {
+            Console.Clear();
+            Console.WriteLine("What would you like to do with " + pet.Name + "?");
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Feed");
+            Console.WriteLine("3. Sleep");
+            Console.WriteLine("4. Bathroom");
+            Console.WriteLine("5. Give water");
+            Console.WriteLine("6. Go Back to Main Menu");
+        }
+
+        static void PetStatus()
+        {
+            Console.Clear();
+            DrawPicture();
             Console.WriteLine("Fatigue: " + pet.Fatigue);
             Console.WriteLine("Hunger: " + pet.Hunger);
             Console.WriteLine("Thirst: " + pet.Thirst);
             Console.WriteLine("Boredom: " + pet.Boredom);
             Console.WriteLine("Potty: " + pet.Potty);
+            Console.WriteLine(Environment.NewLine);
             Console.WriteLine("Press ENTER to return to the Main Menu.");
             Console.ReadKey();
             MainMenu();
@@ -44,7 +82,9 @@ namespace VirtualPetsAmok
             Console.Clear();
             Console.WriteLine("========= MAIN MENU =========");
             Console.WriteLine("1. Display Pet Info");
-            Console.WriteLine("2. Quit Game");
+            Console.WriteLine("2. Pet Interactions");
+            Console.WriteLine("3. Pet Status");
+            Console.WriteLine("5. Quit Game");
 
             string menuSelection = Console.ReadLine();
 
@@ -54,8 +94,42 @@ namespace VirtualPetsAmok
                     PetInfo();
                     break;
                 case "2":
+                    PetInteractions();
+                    break;
+                case "3":
+                    PetStatus();
+                    break;
+                case "5":
                     Environment.Exit(0);
                     break;
+            }
+        }
+
+        static void DrawPicture()
+        {
+            if (pet.Species == "Dog")
+            {
+                // Print Dog
+                Console.WriteLine("    ___");
+                Console.WriteLine(" __/_  `.  .-''' -.");
+                Console.WriteLine(@" \_,` | \-'  /   )`-')");
+                Console.WriteLine(@"  '') `'`    \  ((`'`");
+                Console.WriteLine(" ___Y  ,    .'7 /|");
+                Console.WriteLine("(_,___/...-` (_/_/");
+                Console.WriteLine();
+                Console.WriteLine();
+
+            } else if (pet.Species == "Cat")
+            {
+                // Print cat
+                Console.WriteLine(@"   |\---/|");
+                Console.WriteLine(@"   | ,_, |");
+                Console.WriteLine(@"    \_`_/-..----.");
+                Console.WriteLine(@" ___/ `   ' ,''+ \ ");
+                Console.WriteLine(@"(__...'   __\    |`.___.';");
+                Console.WriteLine(@"  (_,...'(_,.`__)/'.....+");
+                Console.WriteLine();
+                Console.WriteLine();
             }
         }
 
